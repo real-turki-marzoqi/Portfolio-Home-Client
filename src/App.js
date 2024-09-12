10bf3018537ea493856933d05f713142f0f84e4f
real-turki-marzoqi <turki.marzoqi.personal@gmail.com>
2024-09-12 15:42:01 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -6,19 +6,12 @@ import "./Styles/Home/ProfileImage.css";
 import "./Styles/Home/ContactWithMe.css";
 import Home from "./Pages/Home";
 
-//imports Admin
-import Admin from './Pages/Admin'
-import './Styles/Admin/PersonalInfoemations.css'
-import './Styles/Admin/admin.css'
-import './Styles/Admin/texts.css'
-import './Styles/Admin/messages.css'
 
 // Utility Components and Styles Imports
 import NavBar from "./components/NavBar";
 import "./Styles/utility/Footer.css";
 import "./Styles/utility/NavBar.css";
 
-import './Styles/utility/SideBar.css'
 
 // ROUTER
 import { Route, Routes,  } from "react-router-dom";
@@ -29,7 +22,7 @@ function App() {
     <NavBar />
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/-admin-yonitci-/*" element={<Admin/>}/>
+    
     </Routes>
    {/*  <Footer /> */}
   </div>
